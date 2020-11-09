bbbe5da389fbd45d73ec3597436b177e1d7e5ed6
allanrod7 <74158766+allanrod7@users.noreply.github.com>
2020-11-08 22:33:16 -0300

    Add files via upload

---
+++ JavaApplication13/src/POO/Janela.java
@@ -0,0 +1,23 @@
+package POO;
+
+import javax.swing.JFrame;
+
+public class Janela {
+	
+	public static void main(String[] args) {
+	
+		JFrame frame = new JFrame("VELHA ADS");
+		frame.setSize(600, 630);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		
+		JogoDaVelha velha = new JogoDaVelha();
+		velha.setBounds(0, 0, 600, 600);
+		frame.add(velha);
+		
+		frame.addMouseListener(velha);
+	}
+	
+}
